3c3b7694d1346972d97919344ba12fa391ec55b9
axellervik <axel.lervikgmail.com>
2023-12-02 13:45:25 +0100

    HMM1 start

--- Lab 2 - HMM/HMM1/hmm1.py
+++ Lab 2 - HMM/HMM1/hmm1.py
@@ -13,6 +13,32 @@ def print_matrix(matrix):
     [print(matrix[i,j], end=' ') for i in range(dims[0]) for j in range(dims[1])]
     print('\n')
 
-T = parse(input())
-E = parse(input())
-S = parse(input())
+A = parse(input())
+B = parse(input())
+pi = parse(input())
+
+def forward(A, B, pi, sequence):
+    N = A.shape[0] # num states
+    alpha = np.matrix(np.zeros((len(sequence), N)))
+    alpha[0, 0] = pi * B[: , sequence[0]]
+
+    for t in range(1, len(sequence)):
+        for i in range(N):
+            alpha[t, i] = alpha[t-1].dot(A[:, i]) * B[i, sequence[t]]
+
+    return alpha[-1, sequence[-1]]
+
+    # p = 1.0
+    # N = A.shape[0] # num states
+    # alpha = np.matrix(np.zeros((len(sequence), N)))
+    # alpha[0, 0] = pi * B[:, sequence[0]]
+    # for t in range(1, len(sequence)):
+    #     probability = 0.0
+    #     for i in range(N):
+    #         alpha[t, i] = alpha[t-1].dot(A[:, i]) * B[i, sequence[t]]
+    #         probability += alpha[t, i]
+    #     p *= probability
+    # return alpha[-1].sum()
+
+sequence = list(map(int,input().split()))
+print(forward(A, B, pi, sequence[1:]))
