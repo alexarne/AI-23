2e0cefcfdec5af418831ab2b0c3b01cbc2b1cf5b
axellervik <axel.lervikgmail.com>
2023-12-02 11:35:20 +0100

    Solve HMM0, bit scuffed and probably slow printing function but good enough

--- Lab 2 - HMM/HMM0/hmm0.py
+++ Lab 2 - HMM/HMM0/hmm0.py
@@ -1,2 +1,20 @@
 import numpy as np
+ 
+def parse(line):
+    line = line.split()
+    rows = int(line[0])
+    cols = int(line[1])
+    data = list(map(float, line[2:]))
+    return np.matrix([data[i*cols : (i+1)*cols] for i in range(rows)])
 
+def print_matrix(matrix):
+    dims = matrix.shape
+    print(dims[0], dims[1], end=' ')
+    [print(matrix[i,j], end=' ') for i in range(dims[0]) for j in range(dims[1])]
+    print('\n')
+
+T = parse(input())
+E = parse(input())
+S = parse(input())
+
+print_matrix(S*T*E)
