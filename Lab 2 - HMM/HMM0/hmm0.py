d9ae65505e0f39f70501119b47140936d9c90daa
axellervik <axel.lervikgmail.com>
2023-12-02 13:45:33 +0100

    Remove numpy

--- Lab 2 - HMM/HMM0/hmm0.py
+++ Lab 2 - HMM/HMM0/hmm0.py
@@ -1,20 +1,22 @@
-import numpy as np
- 
 def parse(line):
     line = line.split()
     rows = int(line[0])
     cols = int(line[1])
     data = list(map(float, line[2:]))
-    return np.matrix([data[i*cols : (i+1)*cols] for i in range(rows)])
+    return [data[i*cols : (i+1)*cols] for i in range(rows)]
+
+def matmul(A, B):
+    return [[sum(i * j for i, j in zip(r, c)) for c in zip(*B)] for r in A]
 
 def print_matrix(matrix):
-    dims = matrix.shape
-    print(dims[0], dims[1], end=' ')
-    [print(matrix[i,j], end=' ') for i in range(dims[0]) for j in range(dims[1])]
-    print('\n')
+    rows = len(matrix)
+    cols = len(matrix[0])
+    print(rows, cols, end=' ')
+    [print(matrix[i][j], end=' ') for i in range(rows) for j in range(cols)]
+    print()
 
 T = parse(input())
 E = parse(input())
 S = parse(input())
 
-print_matrix(S*T*E)
+print_matrix(matmul(S, matmul(T, E)))
