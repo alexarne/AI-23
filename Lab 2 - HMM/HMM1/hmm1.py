e41122d72aa01d2c17f88df3187025575bbab163
axellervik <axel.lervikgmail.com>
2023-12-02 13:59:43 +0100

    Solve HMM1

--- Lab 2 - HMM/HMM1/hmm1.py
+++ Lab 2 - HMM/HMM1/hmm1.py
@@ -1,32 +1,32 @@
-import numpy as np
- 
 def parse(line):
     line = line.split()
     rows = int(line[0])
     cols = int(line[1])
     data = list(map(float, line[2:]))
-    return np.matrix([data[i*cols : (i+1)*cols] for i in range(rows)])
+    return [data[i*cols : (i+1)*cols] for i in range(rows)]
+
+def matmul(A, B):
+    return [[sum(i * j for i, j in zip(r, c)) for c in zip(*B)] for r in A]
 
 def print_matrix(matrix):
-    dims = matrix.shape
-    print(dims[0], dims[1], end=' ')
-    [print(matrix[i,j], end=' ') for i in range(dims[0]) for j in range(dims[1])]
-    print('\n')
+    rows = len(matrix)
+    cols = len(matrix[0])
+    print(rows, cols, end=' ')
+    [print(matrix[i][j], end=' ') for i in range(rows) for j in range(cols)]
+    print()
 
 A = parse(input())
 B = parse(input())
 pi = parse(input())
 
 def forward(A, B, pi, sequence):
-    N = A.shape[0] # num states
-    alpha = np.matrix(np.zeros((len(sequence), N)))
-    alpha[0, 0] = pi * B[: , sequence[0]]
-
+    N = len(A) # num states
+    alpha = [[0 for _ in range(N)] for _ in range(len(sequence))] # zeros matrix
+    alpha[0] = [pi[0][i] * B[i][sequence[0]] for i in range(N)]
     for t in range(1, len(sequence)):
         for i in range(N):
-            alpha[t, i] = alpha[t-1].dot(A[:, i]) * B[i, sequence[t]]
-
-    return alpha[-1, sequence[-1]]
+            alpha[t][i] = sum([alpha[t-1][j] * A[j][i] for j in range(N)]) * B[i][sequence[t]]
+    return sum(alpha[-1])
 
     # p = 1.0
     # N = A.shape[0] # num states
