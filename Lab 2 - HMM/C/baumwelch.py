2de0dea97390d9e93736d2657d6f63498a946afa
axellervik <axel.lervik@gmail.com>
2023-12-04 16:58:27 +0100

    Answer C questions

--- Lab 2 - HMM/C/baumwelch.py
+++ Lab 2 - HMM/C/baumwelch.py
@@ -11,23 +11,51 @@ def init_matrix(size_y, size_x):
         matrix[i] = [v / rowsum for v in matrix[i]]
     return matrix
 
+def uniform_matrix(size_y, size_x):
+    matrix = [[1/size_x for _ in range(size_x)] for _ in range(size_y)]
+    for i in range(size_y):
+        rowsum = sum(matrix[i])
+        matrix[i] = [v / rowsum for v in matrix[i]]
+    return matrix
+
+def diagonal_matrix(size_y, size_x):
+    matrix = [[0.0 for _ in range(size_x)] for _ in range(size_y)]
+    for i in range(size_y):
+        matrix[i][i] = 1.0
+    for i in range(size_y):
+        rowsum = sum(matrix[i])
+        matrix[i] = [v / rowsum for v in matrix[i]]
+    return matrix
+
+# forward algorithm for validating our model (used when answering Q9)
+def forward(A, B, pi, sequence):
+    N = len(A) # num states
+    alpha = [[0 for _ in range(N)] for _ in range(len(sequence))] # zeros matrix
+    alpha[0] = [pi[0][i] * B[i][sequence[0]] for i in range(N)]
+    for t in range(1, len(sequence)):
+        for i in range(N):
+            alpha[t][i] = sum([alpha[t-1][j] * A[j][i] for j in range(N)]) * B[i][sequence[t]]
+    return sum(alpha[-1])
+
 # backward algorithm - beta-pass algorithm
 # A - Transition matrix
 # B - Emission matrix
 # pi - Initial state vector
 # emissions - Sequence of observations
 # Returns - New estimated transition matrix, emissions matrix, and initial state vector
+PRINT = False
 def baumwelch(A, B, pi, emissions):
     N = len(A)          # num states
     T = len(emissions)  # num observations
 
     oldLogProb = 0
     logProb = 1
-    MAX_ITER = 1000
+    MAX_ITER = 100
     iter = 0
-    while iter < MAX_ITER and abs(oldLogProb - logProb) > 1e-5:
+    while iter < MAX_ITER and abs(oldLogProb - logProb) > 1e-4:
         iter = iter + 1
-        print("Iteration", iter)
+        if PRINT:
+            print("Iteration", iter)
         oldLogProb = logProb
         # Compute all (with alpha and beta normalized)
         norm = [1 for _ in range(T)]
@@ -45,7 +73,7 @@ def baumwelch(A, B, pi, emissions):
 
 
         # --------- beta (normalized) ---------
-        beta = [[0 for _ in range(N)] for _ in range(T)] # zeros matrix
+        beta = [[0 for _ in range(N)] for _ in range(T)] # zeros mat-rix
         beta[-1] = [1 / norm[-1] for _ in range(N)]
         for t in range(T-2, -1, -1):
             for i in range(N):
@@ -75,7 +103,11 @@ def baumwelch(A, B, pi, emissions):
         # Repeat until convergence
         logProb = sum([math.log(norm[i]) for i in range(len(norm))])
         # print("logProb", logProb, "oldLogProb", oldLogProb)
-        print("logprob diff", abs(logProb-oldLogProb))
+        if PRINT:
+            print("logprob diff", abs(logProb-oldLogProb))
+        # print("A:", A)
+        # print("B:", B)
+        # print("pi:", pi)
 
     return A, B, pi
 
@@ -96,14 +128,57 @@ emissions = list(map(int,input().split()))
 # B = init_matrix(3, 4)
 # pi = init_matrix(1, 3)
 
-# Q8 manual initializations
-A = [[0.80, 0.05, 0.15],
-     [0.13, 0.73, 0.04],
-     [0.23, 0.25, 0.52]]
-B = [[0.69, 0.23, 0.08, 0.00],
-     [0.08, 0.42, 0.27, 0.23],
-     [0.03, 0.07, 0.18, 0.72]]
-pi = [[0.7, 0.15, 0.15]]
+# # Q8 manual initializations
+# A = [[0.80, 0.05, 0.15],
+#      [0.13, 0.73, 0.04],
+#      [0.23, 0.25, 0.52]]
+# B = [[0.69, 0.23, 0.08, 0.00],
+#      [0.08, 0.42, 0.27, 0.23],
+#      [0.03, 0.07, 0.18, 0.72]]
+# pi = [[0.7, 0.15, 0.15]]
+
+# Q9
+for N in range(1,6):
+    A = init_matrix(N, N)
+    B = init_matrix(N, 4)
+    pi = init_matrix(1, N)
+    halfway = int(len(emissions)/2)
+    if PRINT:
+        print("N =",N,", BEFORE:")
+        print(A)
+        print(B)
+        print(pi)
+    # print(emissions[1:halfway])
+    # print(emissions[halfway:])
+    A2, B2, pi2 = baumwelch(A, B, pi, emissions[1:halfway])
+    if PRINT:
+        print("N =",N,", AFTER:")
+        print(A2)
+        print(B2)
+        print(pi2)
+    alpha = forward(A2, B2, pi2, emissions[halfway:])
+    print("N =",N,"=> alpha =",alpha)
+
+# # Q10
+# Uniform matrices: 
+# A = uniform_matrix(3, 3)
+# B = uniform_matrix(3, 4)
+# pi = uniform_matrix(1, 3)
+
+# # Diagonal A and specific start state:
+# A = diagonal_matrix(3, 3)
+# print(A)
+# B = init_matrix(3, 4)
+# pi = [[0.0, 0.0, 1.0]]
+
+# # Close to solution:
+# A = [[0.72, 0.04, 0.24],
+#      [0.13, 0.78, 0.09],
+#      [0.21, 0.28, 0.51]]
+# B = [[0.69, 0.22, 0.08, 0.01],
+#      [0.08, 0.42, 0.27, 0.23],
+#      [0.03, 0.07, 0.18, 0.72]]
+# pi = [[0.9, 0.05, 0.05]]
 
 A2, B2, pi2 = baumwelch(A, B, pi, emissions[1:])
 print(A2)
